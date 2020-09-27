8cd1b466bd9d71fcb142819cb39c4290b313c23c
Boston <zhsegal@gmail.com>
2020-09-27 16:15:11 +0300

    working before design

--- public/index.js
+++ public/index.js
@@ -1,21 +1,95 @@
 const testA= `
-<div id="addedQuestions">
-        <h1> Allergy Test A</h1>
-        <div class="form-group row">
-          <label for="Allergen 1" class="col-sm-2 col-form-label">Allergen 1</label>
-          <div class="col-sm-3">
-            <select class="form-control" id="testTypeSelect">
-              <option selected>Choose Allergen Type</option>
-              <option value="testA">Dust</option>
-              <option value="TestB">Peanuts</option>
-              <option value="TestB">Olive Trees</option>
-            </select>
+<div class="mt-5 center-block" id="addedQuestions">
+          <h1> Allergy Test A</h1>
+
+          <div class="form-group row mt-5">
+            <label for="Allergen 1" class="col-sm-2 col-form-label">Allergen 1</label>
+            <div class="col-sm-3">
+              <select class="form-control" id="testTypeSelect" name="allergen1">
+                <option selected value="">Choose Allergen Type</option>
+                <option value="Dust">Dust</option>
+                <option value="Peanuts">Peanuts</option>
+                <option value="OliveTree">Olive Trees</option>
+              </select>
+            </div>
+            <div class="col-sm-3">
+              <input type="text" class="form-control" id="PatientID" placeholder="Allergen Value"
+                name="allergen1_value">
+            </div>
           </div>
-          <div class="col-sm-3">
-            <input type="text" class="form-control" id="PatientID" placeholder="Allergen Value">
+
+          <div class="form-group row mt-5">
+            <label for="Allergen 2" class="col-sm-2 col-form-label">Allergen 2</label>
+            <div class="col-sm-3">
+              <select class="form-control" id="testTypeSelect" name="allergen2">
+                <option selected value="">Choose Allergen Type</option>
+                <option value="Dust">Dust</option>
+                <option value="Peanuts">Peanuts</option>
+                <option value="OliveTree">Olive Trees</option>
+              </select>
+            </div>
+            <div class="col-sm-3">
+              <input type="text" class="form-control" id="PatientID" placeholder="Allergen Value"
+                name="allergen2_value">
+            </div>
+          </div>
+
+          <div class="form-group row mt-5">
+            <label for="Allergen 3" class="col-sm-2 col-form-label">Allergen 3</label>
+            <div class="col-sm-3">
+              <select class="form-control" id="testTypeSelect" name="allergen3">
+                <option selected value="">Choose Allergen Type</option>
+                <option value="Dust">Dust</option>
+                <option value="Peanuts">Peanuts</option>
+                <option value="OliveTree">Olive Trees</option>
+              </select>
+            </div>
+            <div class="col-sm-3">
+              <input type="text" class="form-control" id="PatientID" placeholder="Allergen Value"
+                name="allergen3_value">
+            </div>
           </div>
         </div>
-      </div>
+
+`
+const testB= `
+<div class="mt-5 center-block" id="addedQuestions">
+          <h1> Allergy Test B</h1>
+
+          <div class="form-group row mt-5">
+            <label for="Allergen 1" class="col-sm-2 col-form-label">Allergen 1</label>
+            <div class="col-sm-3">
+              <select class="form-control" id="testTypeSelect" name="allergen1">
+                <option selected value="">Choose Allergen Type</option>
+                <option value="Dust">Dust</option>
+                <option value="Peanuts">Peanuts</option>
+                <option value="OliveTree">Olive Trees</option>
+              </select>
+            </div>
+            <div class="col-sm-3">
+              <input type="text" class="form-control" id="PatientID" placeholder="Allergen Value"
+                name="allergen1_value">
+            </div>
+          </div>
+
+          <div class="form-group row mt-5">
+            <label for="Allergen 2" class="col-sm-2 col-form-label">Allergen 2</label>
+            <div class="col-sm-3">
+              <select class="form-control" id="testTypeSelect" name="allergen2">
+                <option selected value="">Choose Allergen Type</option>
+                <option value="Dust">Dust</option>
+                <option value="Peanuts">Peanuts</option>
+                <option value="OliveTree">Olive Trees</option>
+              </select>
+            </div>
+            <div class="col-sm-3">
+              <input type="text" class="form-control" id="PatientID" placeholder="Allergen Value"
+                name="allergen2_value">
+            </div>
+          </div>
+          
+        </div>
+
 `
 
 
@@ -23,6 +97,9 @@ $('#testTypeSelect').change(function(){
    $("#addedQuestions").remove()
     if($(this).val()==='testA'){
        $("#testselection").after(testA)
-   
    }
+   else if ($(this).val()==='testB'){
+    $("#testselection").after(testB)
+}
+
 })
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 
 
@@ -6,13 +8,50 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+const mongoURIlocal = "mongodb://localhost:27017/ShebaAllergyDB";
+const mongoURI = "mongodb+srv://admin-zvika:5293612aA!@cluster0.w9hbu.mongodb.net/ShebaAllergyDB";
 
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const patientSchema = new mongoose.Schema({
+    PatientID: String,
+    age: String,
+    gender: String,
+    diagnosis: String,
+    allergen1: String,
+    allergen1_value: String,
+    allergen2: String,
+    allergen2_value: String,
+    allergen3: String,
+    allergen3_value: String,
+})
+
+const Patient = mongoose.model("Patient", patientSchema)
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
     res.sendFile('public/index.html', { root: __dirname })
 });
 
+app.post('/send', (req, res) => {
+    var patient=new Patient({
+        PatientID:req.body.PatientID,
+        age:req.body.age,
+        gender:req.body.gender,
+        diagnosis:req.body.diagnosis,
+        allergen1:req.body.allergen1,
+        allergen1_value:req.body.allergen1_value,
+        allergen2:req.body.allergen2,
+        allergen2_value:req.body.allergen2_value,
+        allergen3:req.body.allergen3,
+        allergen3_value:req.body.allergen3_value,
+    })
+    patient.save()
+    console.log(req.body)
+    res.redirect('/')
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
